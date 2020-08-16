fc39b63291e3745cf7ad6f2339ace6d3e1d1f273
priti017 <pritilata.biswas66@gmail.com>
2020-08-16 23:21:33 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,19 +33,33 @@
                     <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                       <div class="modal-dialog modal-lg">
                         <div class="modal-content">
+
                           <form method="POST" action="{{ route('PujaCovid.store') }}" class="col-md-12 " enctype="multipart/form-data">@csrf
+                          
                             <div class="form-group">
                               <label>Name</label>
                               <input type="text" class="form-control" name="name"  placeholder="Name">
                             </div>
+
                             <div class="form-group">
                               <label>Area</label>
-                              <input type="text" class="form-control" name="area"  placeholder="Area">
+                              <select name="area" id="" class="form-control">
+                                  <option value="Cumilla">Cumilla</option>
+                                  <option value="Chittagong">Chittagong</option>
+                                  <option value="Dhaka">Dhaka</option>
+                                  <option value="Rangpur">Rangpur</option>
+                                  <option value="Noakhali">Noakhali</option>
+                                  <option value="Barishal">Barishal</option>
+                                  <option value="Others">Others</option>
+                               </select>
                             </div>
+
                             <div class="form-group">
                               <label>Symptoms</label>
-                              <input type="text" class="form-control" name="symptoms"  placeholder="Symptoms">
+                                  <textarea  name="symptoms" class="form-control" placeholder="symptoms" rows="10" cols="50">
+                                  </textarea>
                             </div>
+
                             <div class="form-group">
                               <label>Date</label>
                               <input type="date" class="form-control" name="date">
